dd68470e733798efd85eeaa31ec6010b650a94d5
Chewsday <z647623@gmail.com>
2025-02-15 11:09:11 +0800

    week5-4

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -67,7 +67,7 @@ function Form () {
             })}
             id="tel"
             name="tel"
-            type="text"
+            type="tel"
             className={`form-control ${errors.tel && 'is-invalid'}`}
             placeholder="請輸入電話" />
 
